10f3437fd93eb875efe966c970fe8e3b352fcb21
unknown <Administrator@PC2-11.bijisen.local>
2025-02-04 15:12:35 +0900

    ボタンクリック後の現在地を設定しました

--- app/controllers/preview_controller.rb
+++ app/controllers/preview_controller.rb
@@ -1,2 +1,34 @@
 class PreviewController < ApplicationController
+    def prevQ
+        prevpanlist
+      end
+
+      def prevA
+      prevpanlist
+      end
+
+      private
+      def prevpanlist
+        puts "#########prevC!!!!!!!!!!!!###########"
+        puts params
+        case params[:action_type]
+          #///////////////////ここから問題一覧内のボタン////////////////
+    
+          when 'createQ'
+            @@message2 = '問題作成'
+          when 'prevQ'
+            @@message2 = '問題詳細'
+          when 'prevA'
+            @@message2 = '解答詳細'
+    
+          #//////////////////////////////////////////////////////////
+    
+         
+        end
+          puts @@message
+          @title = @@message
+          @title2 = @@message2
+    end
+      
+    
 end
--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,5 +1,23 @@
 class TopController < ApplicationController
     def index
-        
+        puts "####index######################"
+    puts params
+    case params[:action_type]
+    when 'startA'
+        @@message = '基本情報技術者試験（A試験）/試験実施'
+    when 'listA'
+        @@message = '基本情報技術者試験（A試験）/問題詳細'
+    when 'startB'
+        @@message = '基本情報技術者試験（B試験）/試験実施'
+    when 'listB'
+        @@message = '基本情報技術者試験（B試験）/問題詳細'
+    when 'startS'
+        @@message = 'Ruby Silver/試験実施'
+    when 'listS'
+        @@message = 'Ruby Silver/問題詳細'
+    
     end
+    puts @@message
+    end
+    
 end
